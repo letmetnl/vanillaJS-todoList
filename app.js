ed4c381f1ede30af4b5a2088d0cc992f963255c0
mohits Projects <mohit5150515@gmail.com>
2021-01-12 20:51:20 +0530

    added some default task onclick

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 // selectors
-const todoInput = document.querySelector("todo-input");
-const todoButton = document.querySelector("submit-btn");
-const todoList = document.querySelector("todo-list");
-
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".submit-btn");
+const todoList = document.querySelector(".todo-list");
 
 // Event Listeners
-todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener("click", addTodo);
 
 // tasks or functions to be performed on actions
-addTodo = (e) => {
-    // preventing default submitting of form
-    e.preventDefault();
-    // creating a div to contain todo list
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo');
+function addTodo(event) {
+  // preventing default submitting of form
+  event.preventDefault();
+  // creating a div to contain todo list
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
-    // creating the list item
-    const newTask = document.createElement('li');
-    newTask.innerHTML=todoInput;
-    newTask.classList.add('list-item');
+  // creating the list item
+  const newTask = document.createElement("li");
+  newTask.innerText = "new Task";
+  newTask.classList.add("list-item");
+  todoDiv.appendChild(newTask);
 
-    // creating the buttons for todo-item
-    const 
+  // creating the buttons for todo-item
+  // checkmark button
+  const taskCompleted = document.createElement("button");
+  taskCompleted.innerHTML = '<i class="fas fa-check"></i>';
+  taskCompleted.classList.add("complete-btn");
+  todoDiv.appendChild(taskCompleted);
 
+  //Delete button
+  const deleteTask = document.createElement("button");
+  deleteTask.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteTask.classList.add("delete-btn");
+  todoDiv.appendChild(deleteTask);
+
+  // append this complete div to todolist in html
+  todoList.appendChild(todoDiv);
 }
